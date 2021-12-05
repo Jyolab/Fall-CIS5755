8963d04c521fe96a7dc5aeb021903a038e554054
EC2 Default User <ec2-user@ip-172-31-80-21.ec2.internal>
2021-12-05 01:56:20 +0000

    example 2 with flask

--- Chapter-1-Introduction/ex2-flask.py
+++ Chapter-1-Introduction/ex2-flask.py
@@ -1,8 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
-def hello_world():
-   return 'Hello World'
+def login():
+   return 'Jyothsna Korada and Sahithi Talasila'
    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug = True)
